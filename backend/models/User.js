139c3847053797395bfa5d155674a8612ffb5fee
Priya <priyapatel.892002@gmail.com>
2023-08-29 23:27:59 +0530

    video 44 completed

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const UserSchema = new Schema({
@@ -21,4 +22,29 @@ const UserSchema = new Schema({
 
 })
 
+=======
+const mongoose = require('mongoose');
+
+const UserSchema = new Schema({
+    name : {
+        type:String,
+        required : true
+    },
+    email :{
+        type : String,
+        required : true,
+        unique : true
+    },
+    password :{
+        type : String ,
+        required : true//password is a string
+    },
+    date : {
+        type : Date,
+        default : Date.now
+    }
+
+})
+
+>>>>>>> 14ac03c (module and routes files are added)
 module.exports = mongoose.model('user' , UserSchema);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express')
 const router =  express.Router();
 
@@ -8,4 +9,16 @@ router.get('/' , (req , res) =>{
     }
    res.json(obj)
 })
+=======
+const express = require('express')
+const router =  express.Router();
+
+router.get('/' , (req , res) =>{
+    obj = {
+        a : 'this',
+        number : 34
+    }
+   res.json(obj)
+})
+>>>>>>> 14ac03c (module and routes files are added)
 module.exports = router;
