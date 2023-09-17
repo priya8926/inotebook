b7c6c362cbc29ab5fed32f9935dcd1728ee4a924
Priya <priyapatel.892002@gmail.com>
2023-09-17 21:27:01 +0530

    delete notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -67,4 +67,31 @@ try {
    res.status(500).send("Internal server error");
 }
 })
+
+/// Router 4:Delete an existing Note using: DELETE "/api/notes/deletenote" no login required
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+try {
+   const { title, description, tag } = req.body;
+   //Create a newNote object
+   const newNote = {};
+   if(title){newNote.title = title};
+   if(description){newNote.description = description};
+   if(tag){newNote.tag = tag};
+
+   // find the note to be delete and deleted it 
+   let note = await Note.findById(req.params.id);
+   if(!note ){
+       return res.status(404).send("Not Found")
+   }
+   //Allow deletion if user own this note
+   if(note.user.toString() !== req.user.id){
+      return res.status(401).send("Not Allowed")
+   }
+   note = await Note.findByIdAndDelete(req.params.id) 
+   res.json({"sucess" : "Note has been deleted sucessfully" , note:note})
+} catch (error) {
+   console.error(error.message);
+   res.status(500).send("Internal server error");
+}
+})
 module.exports = router;
