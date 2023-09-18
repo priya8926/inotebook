74115fe36fd23f73742bdd378ef62db1b8d70300
Priya <priyapatel.892002@gmail.com>
2023-09-18 23:36:10 +0530

    context API

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import noteContext from '../context/notes/noteContext'
 
 const About = () => {
+  const a = useContext(noteContext)
+  useEffect(() =>{
+    a.update()
+  },[])
   return (
     <div>
-      this is about
+      this is about {a.state.name} and she is in class {a.state.class}
     </div>
   )
 }
