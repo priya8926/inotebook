5fd3f29c42ba3e961b2a5f4c4a33da8812a7bf94
Priya <priyapatel.892002@gmail.com>
2023-09-23 18:07:53 +0530

    Updating notes on front-end

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -6,32 +6,6 @@ const NoteState = (props) => {
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
-  //Add a Note
-  const addNote = async (title, description, tag) => {
-    //todo: API call
-    //API call
-    const response = await fetch(`${host}/api/notes/addnote`, {
-      method: 'POST',
-      headers: {
-        'content-Type': 'application/json',
-        'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjUwNmZhOGMyNjNiYmM5YWRmZTBlZjA1In0sImlhdCI6MTY5NDk1NjE5Mn0.x36pO4oX6EzanQj9BJbR9ow6jfT_yx9D2SxP0RpGCKU'
-      },
-      body: JSON.stringify({ title, description, tag })
-    });
-    const json = response.json();
-    console.log('Adding a new note')
-    const newNote = {
-      "_id": "65071227f61bddff49535700b4b",
-      "user": "6506fa8c263bbc9adfe0ef05",
-      "title": title,
-      "description": description,
-      "tag": tag,
-      "date": "2023-09-17T14:50:15.345Z",
-      "__v": 0
-    };
-    // setNotes(notes.concat(notes))
-    setNotes([...notes, newNote])
-  }
 
   //Get all  Note
   const getNotes = async () => {
@@ -49,6 +23,34 @@ const NoteState = (props) => {
     setNotes(json)
 
   }
+  //Add a Note
+  const addNote = async (title, description, tag) => {
+    //todo: API call
+    //API call
+    const response = await fetch(`${host}/api/notes/addnote`, {
+      method: 'POST',
+      headers: {
+        'content-Type': 'application/json',
+        'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjUwNmZhOGMyNjNiYmM5YWRmZTBlZjA1In0sImlhdCI6MTY5NDk1NjE5Mn0.x36pO4oX6EzanQj9BJbR9ow6jfT_yx9D2SxP0RpGCKU'
+      },
+      body: JSON.stringify({ title, description, tag })
+    });
+    const json = await response.json();
+    console.log(json)
+
+    console.log('Adding a new note');
+    const newNote = {
+      "_id": "65071227f61bddff49535700b4b",
+      "user": "6506fa8c263bbc9adfe0ef05",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": "2023-09-17T14:50:15.345Z",
+      "__v": 0
+    };
+    // setNotes(notes.concat(notes))
+    setNotes([...notes, newNote])
+  }
   //delete a Note
   const deleteNote = async (id) => {
     //todo: API call
@@ -61,8 +63,6 @@ const NoteState = (props) => {
     });
     const json = await response.json();
     console.log(json);
-
-    console.log("delete a note with id" + id)
     const newNote = notes.filter((notes) => { return notes._id !== id })
     setNotes(newNote)
 
@@ -71,24 +71,28 @@ const NoteState = (props) => {
   const editNote = async (id, title, description, tag) => {
     //API call
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: 'POST',
+      method: 'PUT',
       headers: {
         'content-Type': 'application/json',
         'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjUwNmZhOGMyNjNiYmM5YWRmZTBlZjA1In0sImlhdCI6MTY5NDk1NjE5Mn0.x36pO4oX6EzanQj9BJbR9ow6jfT_yx9D2SxP0RpGCKU'
       },
       body: JSON.stringify({ title, description, tag })
     });
-    const json = response.json();
-    // login to edit in client
+    const json = await response.json();
+    console.log(json)
 
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    let newNotes = JSON.parse(JSON.stringify(notes))
+    // Logic to edit in client
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
     }
+    setNotes(newNotes);
   }
   return (
     <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>
